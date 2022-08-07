88248644c64bc454066a575ac14660b264aab6b3
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-08-07 18:45:29 -0500

    Create Command and Platform models along with their relavant dtos

---
+++ CommandService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+using CommandService.Models;
+
+namespace CommandService.Data;
+
+public interface ICommandRepo
+{
+    // Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform platform);
+    bool PlatformExists(int platformId);
+    
+    // Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command? GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+
+    bool SaveChanges();
+}
