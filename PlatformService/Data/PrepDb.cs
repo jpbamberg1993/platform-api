17b3f5b818445ec5e07ef20211d438a5a59a5a47
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-08-07 13:52:58 -0500

    Run migrations in Postgres DB if production

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,17 +1,32 @@
-﻿using PlatformService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
 
 namespace PlatformService.Data;
 
 public static class PrepDb
 {
-    public static void PrepPopulation(IApplicationBuilder app)
+    public static void PrepPopulation(IApplicationBuilder app, bool isProduction)
     {
         using var serviceScope = app.ApplicationServices.CreateScope();
-        SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()!);
+        SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()!, isProduction);
     }
 
-    private static void SeedData(AppDbContext context)
+    private static void SeedData(AppDbContext context, bool isProduction)
     {
+        if (isProduction)
+        {
+            Console.WriteLine("--> Attempting to apply migrations.");
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not run migrations.");
+                Console.WriteLine(e);
+            }
+        }
+
         if (!context.Platforms.Any())
         {
             Console.WriteLine("--> Seeding data...");
