b23d9e7c66a3c8a98909b5cdc52ef12758e422cc
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-08-11 07:18:01 -0500

    Create PlatformPublishDto

--- CommandService/Profiles/CommandsProfile.cs
+++ CommandService/Profiles/CommandsProfile.cs
@@ -11,5 +11,7 @@ public class CommandsProfile : Profile
         CreateMap<Platform, PlatformReadDto>();
         CreateMap<CommandCreateDto, Command>();
         CreateMap<Command, CommandReadDto>();
+        CreateMap<PlatformPublishDto, Platform>()
+            .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.Id));
     }
 }
