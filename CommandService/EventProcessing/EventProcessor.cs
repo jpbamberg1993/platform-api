f29a6f2789de2c3439d04af00956946cb3313a93
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-08-11 07:18:33 -0500

    Set up initial message bus even processing

---
+++ CommandService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,84 @@
+using System.Formats.Asn1;
+using System.Text.Json;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
+using CommandService.Models;
+
+namespace CommandService.EventProcessing;
+
+public class EventProcessor : IEventProcessor
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly IMapper _mapper;
+
+    public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+    {
+        _scopeFactory = scopeFactory;
+        _mapper = mapper;
+    }
+    
+    public void ProcessEvent(string message)
+    {
+        var eventType = DetermineEvent(message);
+
+        switch (eventType)
+        {
+            case EventType.PlatformPublished:
+                // todo
+                break;
+            default:
+                break;
+        }
+    }
+
+    private EventType DetermineEvent(string notificationMessage)
+    {
+        Console.WriteLine("--> Determining event.");
+
+        var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+        switch (eventType.Event)
+        {
+            case "Platform_Published":
+                Console.WriteLine("--> Platform published event detected.");
+                return EventType.PlatformPublished;
+            default:
+                Console.WriteLine("--> Could not determine event type.");
+                return EventType.Undetermined;
+        }
+    }
+
+    private void AddPlatform(string platformPublishMessage)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var repo = scope.ServiceProvider.GetRequiredService<ICommandRepo>();
+
+        var platformPublishDto = JsonSerializer.Deserialize<PlatformPublishDto>(platformPublishMessage);
+
+        try
+        {
+            var platform = _mapper.Map<Platform>(platformPublishDto);
+            if (!repo.PlatformExistsWithExternalId(platform.ExternalId))
+            {
+                repo.CreatePlatform(platform);
+                repo.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> Platform already exists.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"--> Could not add Platform to db. {e.Message}");
+            Console.WriteLine(e);
+        }
+    }
+}
+
+public enum EventType
+{
+    PlatformPublished,
+    Undetermined,
+}
