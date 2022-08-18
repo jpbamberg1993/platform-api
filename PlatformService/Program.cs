9a13c05126482e6bd811345ee122b7379ac77160
J Paul Bamberg <jpbamberg1993@gmail.com>
2022-08-18 08:03:54 -0500

    Enable Grpc to connect to localhost

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -17,12 +17,14 @@ if (builder.Environment.IsProduction())
 else
 {
     Console.WriteLine("Using in memory database.");
-    builder.Services
+    builder
+        .Services
         .AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
     
     builder.WebHost.ConfigureKestrel(options =>
     {
-        options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+        options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http1);
+        options.ListenLocalhost(5001, o => o.Protocols = HttpProtocols.Http2);
     });
 }
 
